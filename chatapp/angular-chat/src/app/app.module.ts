5923150df689735c334081c6e4fb3fad96bcc821
Zainab Lawal <zainablaw1012@gmail.com>
2021-08-01 21:55:40 +0300

    Editing the join page however it's not functional due to abug.

--- chatapp/angular-chat/src/app/app.module.ts
+++ chatapp/angular-chat/src/app/app.module.ts
@@ -1,5 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router'; // for routing
+import { HttpClientModule } from '@angular/common/http'; // for the http client
+import { FormsModule } from '@angular/forms'; // to handle forms
 
 import { AppComponent } from './app.component';
 import { JoinComponent } from './join/join.component';
@@ -7,12 +11,17 @@ import { JoinComponent } from './join/join.component';
 @NgModule({
   declarations: [
     AppComponent,
-    JoinComponent
+    JoinComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'join', component: JoinComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- chatapp/angular-chat/src/app/state.service.ts
+++ chatapp/angular-chat/src/app/state.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class StateService {
+export declare interface User {
+  token: string;
+  apiKey: string;
+  username: string;
+}
 
-  constructor() { }
+@Injectable({
+  providedIn: 'root',
+})
+
+export class StateService {
+  constructor() {}
+
+  private _user: User;
+
+  get user(): User {
+    return this._user;
+  }
+
+  set user(user: User) {
+    this._user = user;
+  }
 }
