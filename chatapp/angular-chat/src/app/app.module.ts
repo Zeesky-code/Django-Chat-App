a041f3d95835779d80a1fc3fbb5b7f19c2f7d1d9
Zainab Lawal <zainablaw1012@gmail.com>
2021-08-05 23:03:10 +0300

    Chat view created but problem with Angular code

--- chatapp/angular-chat/src/app/app.module.ts
+++ chatapp/angular-chat/src/app/app.module.ts
@@ -7,18 +7,21 @@ import { FormsModule } from '@angular/forms'; // to handle forms
 
 import { AppComponent } from './app.component';
 import { JoinComponent } from './join/join.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JoinComponent,
+    ChatComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-    { path: 'join', component: JoinComponent },
+      { path: '', component: ChatComponent },
+      { path: 'join', component: JoinComponent },
 ], {
     initialNavigation: 'enabled'
 }),
--- chatapp/angular-chat/src/app/stream.service.ts
+++ chatapp/angular-chat/src/app/stream.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { StreamChat, Channel, ConnectAPIResponse } from 'stream-chat';
+
+declare interface UserInfo {
+  token: string;
+  apiKey: string;
+  username: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class StreamService {
 
-  constructor() { }
+  constructor() {}
+  streamClient!: StreamChat;
+  currentUser!: ConnectAPIResponse;
+  
+
+  public async initClient(user: UserInfo): Promise<Channel> {
+    this.streamClient = new StreamChat(user.apiKey);
+    this.currentUser = await this.streamClient.setUser(
+      {
+        id: user.username,
+        name: user.username,
+      },
+      user.token
+    );
+    return this.streamClient.channel('messaging', 'General');
+  }
 }
